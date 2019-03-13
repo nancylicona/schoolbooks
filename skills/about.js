aa407aefad9427e192038b536d548b01b7558b9d
nancylicona <32726663+nancylicona@users.noreply.github.com>
2019-03-13 11:07:28 -0500

    Update about.js

--- skills/about.js
+++ skills/about.js
@@ -9,16 +9,16 @@ module.exports = function (controller, bot) {
     var botcommons = {
 
         // Bot description
-        "description": "It's an awesome bot for sure!",
+        "description": "I help you keep up with your school ebooks!",
 
         // Where to get more information about the bot
-        "url": "https://github.com/CiscoDevNet/botkit-template",
+        //"url": "https://github.com/CiscoDevNet/botkit-template",
 
         // Legal owner
-        "legal_owner": "Cisco DevNet <https://developer.cisco.com>",
+        "legal_owner": "Nancy Licona",
 
         // Contact name for support
-        "support_contact": "Stève Sfartz <mailto:stsfartz@cisco.com>",
+        "support_contact": "Nancy Licona <nanlicona@utexas.edu>",
 
         // Messaging platform
         // [WORKAROUND] overriding Botkit's integrated support temporarly as 'ciscospark' is still returned
